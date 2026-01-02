dbd0a6317d9b8abf6aa0991ddc1065e205db47e8
Naitik Kumar <naitikkuma6654@gmail.com>
2026-01-02 18:27:23 +0530

    function practice

---
+++ JavaScript/script.js
@@ -0,0 +1,17 @@
+//Revising the functions 
+function greet(){
+    console.log("Happy New Year Guys");
+}; 
+
+greet();
+
+function sum(a, b){
+    console.log(a+b);
+}
+
+sum(10,12);
+
+//fat arrow functions
+let fnc = () => {
+    console.log("This Is a fat arrow function")
+};
